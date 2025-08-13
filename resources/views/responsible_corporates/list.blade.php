d581261ca2bfb178fa3c904223a9070355400ca6
Shivani-budtech <sujata.mody@envesya.com>
2025-08-13 15:29:42 +0530

    change inde file path

--- resources/views/responsible_corporates/list.blade.php
+++ resources/views/responsible_corporates/list.blade.php
@@ -11,6 +11,9 @@
       <div class="card-body">
          <div class="row clear">
             <div class="col-lg-12">
+               <div class="d-flex justify-content-end mb-2">
+                  <a href="{{ route('responsible-corp-add') }}" class='btn btn-primary action-btn'>Add</a>
+               </div>
                <div class="table-responsive">
                   <table id="" class="table table-striped table-bordered category-tbl freelist_tbl">
                      <thead>
