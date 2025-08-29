736d561d8d3a0767b9db07ceb6dab6262b354f43
Shivani-budtech <sujata.mody@envesya.com>
2025-08-29 12:36:12 +0530

    log

--- app/Http/Controllers/ResponsibleCorporatesController.php
+++ app/Http/Controllers/ResponsibleCorporatesController.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redirect;
 
 class ResponsibleCorporatesController extends Controller
