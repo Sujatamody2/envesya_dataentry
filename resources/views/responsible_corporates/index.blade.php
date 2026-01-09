e4248125ca2282b9ce2c3270a29e7609c60ace6e
Sujatamody2 <107045945+Sujatamody2@users.noreply.github.com>
2026-01-09 14:42:40 +0530

    fix

--- resources/views/responsible_corporates/index.blade.php
+++ resources/views/responsible_corporates/index.blade.php
@@ -55,7 +55,7 @@
                                     <a href="{{ route('responsible-corp-update', [$corporate->id, 'page' => request()->query('page', '')]) }}" class='btn btn-success action-btn'>Edit</a>
                                     @if(auth()->user()->is_admin == 1)
                                     
-                                    <a href="{{ route('responsible-corp-delete',$corporate->id}}" class='btn btn-danger'>Delete</a>
+                                    <a href="{{ route('responsible-corp-delete',$corporate->id) }}" class='btn btn-danger'>Delete</a>
                                     @endif
                                     @if(auth()->user()->is_admin == 1)
                                        @if($corporate->approval == 2)
